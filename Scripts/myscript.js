da389e30ae763eb3b811ac6a010a0bc31c54773c
Amardeep Agrawal <ethenhunt321@outlook.com>
2018-06-29 14:06:32 +0530

    First Release
    First Release

---
+++ Scripts/myscript.js
@@ -0,0 +1,18 @@
+﻿function LoadHeaderFooter() {
+    $(document).ready(function () {
+        $("#Header").load("./Header.html");
+        $("#Footer").load("./Footer.html");
+    });
+}
+
+function PlayPause() {
+        $("#playbutton").click(function () {
+            $("#mycarousel").carousel('cycle');
+        });
+
+        $("#pauseButton").click(function () {
+            $("#myCarousel").carousel("pause");
+        });
+}
+
+
